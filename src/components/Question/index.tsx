23c088cefb864e521a09b1d9b7e536c437c67650
Rodrigo Vitoriense <rodrigovitoriense@gmail.com>
2021-06-25 09:27:52 -0300

    update aula 4 part 2

--- src/components/Question/index.tsx
+++ src/components/Question/index.tsx
@@ -1,3 +1,5 @@
+import {ReactNode} from 'react';
+
 import './styles.scss';
 
 type QuestionProps = {
@@ -5,14 +7,14 @@ type QuestionProps = {
   author: {
     name: string;
     avatar:string;
-  }
+  };
+  children?: ReactNode;
 }
-
-
 
 export function Question({
   content,
   author,
+  children,
 }: QuestionProps) {
   return (
     <div className="question">
@@ -23,7 +25,7 @@ export function Question({
           <span>{author.name}</span>
         </div>
         <div>
-          
+          {children}
         </div>
       </footer>
     </div>
